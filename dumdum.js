82c1fad9293d4c688360a56526ada4aac2f82bcb
praveenagowtham <gowthamsathiya1991@gmail.com>
2016-11-18 08:29:51 -0600

    Updating double quotes font

--- dumdum.js
+++ dumdum.js
@@ -3,28 +3,27 @@
  @link: https://github.com/praveenagowtham/ourwedding
 */
 (function () {
-	var Gowtham = new Person(“Gowtham”),
-	    Praveena = new Person(“Praveena”);
+ var Gowtham = new Person("Gowtham"),
+     Praveena = new Person("Praveena");
 
-	gettingMarried({
-		boy: Gowtham,
-		girl: Praveena,
-		in: V.S.P. Thirumana Mahal, Kallakurichi
-		on: “4, 5th Dec, 2016”
-		at: “ Dec 4 - Reception: 6 pm onwards; 
-			Dec 5 - Wedding: 9:15 AM - 10:15 AM”
-    });
-	
-	function Person(name) {
-		this.name = name;
-		this.status = “single”; 
-    }
+ gettingMarried({
+  boy: Gowtham,
+  girl: Praveena,
+  in: "V.S.P. Thirumana Mahal, Kallakurichi",
+  on: "4, 5th Dec, 2016",
+  at: "Dec 4 - Reception: 6 pm onwards; Dec 5 - Wedding: 9:15 AM - 10:15 AM"
+ });
+ 
+ function Person(name) {
+  this.name = name;
+  this.status = "single"; 
+ }
 
-	function gettingMarried(args) {
-		args.boy.status = args.girl.status = “committed”;
-		console.log(“Expecting you with your family in “ + args.in +
-				   “ on “ + args.on + “ at “ + args.at + “\n” +
-				   “Instead of compile, give your best compliments”);
-    }
+ function gettingMarried(args) {
+  args.boy.status = args.girl.status = "committed";
+  console.log("Expecting you with your family in " + args.in +
+       " on " + args.on + " at " + args.at + "\n" +
+       "Instead of compile, give your best compliments");
+ }
     
-}());	
+}()); 
